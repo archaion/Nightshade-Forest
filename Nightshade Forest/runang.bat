918277105672590bf72ebdbdee6594a76ed71baa
archaion <night@pointedulac.net>
2025-01-05 09:40:11 -0600

    Initial upload

---
+++ Nightshade Forest/runang.bat
@@ -0,0 +1,4 @@
+@echo off
+rem Automatically generated.
+rem Run the game using the most recent variant and savefile.
+start angband.exe -variant AngbandTk -savefile "C:\Users\Xunxi Wu\Documents\! PROJECTS\!!!ATELIER VAMPYRE\Nightshade Forest\! NIGHTSHADE FOREST\Nightshade Forest\variant\AngbandTk\lib\save\R" %1 %2 %3 %4 %5 %6 %7 %8
